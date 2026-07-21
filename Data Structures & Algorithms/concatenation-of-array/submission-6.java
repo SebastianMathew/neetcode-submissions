058ac3400da62d4a394e0c93bd5fd56e527ff293
SebastianMathew <139248438+SebastianMathew@users.noreply.github.com>
2026-07-21 09:27:10 +0530

    Add: concatenation-of-array - submission-6

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-6.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] a = new int[n*2];
+        for(int i = 0; i<n*2;i++)
+        {
+            a[i] = nums[i%n];
+        }
+        return a;
+    }
+}
