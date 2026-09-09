077a6b16882f38e75291431620f72eea12db3728
SebastianMathew <139248438+SebastianMathew@users.noreply.github.com>
2026-09-09 13:05:26 +0530

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = defaultdict(int)
+        for i in range(len(nums)):
+            need = target - nums[i]
+
+            if need in seen:
+                return [seen[need],i]
+
+            seen[nums[i]] = i
+            
+
+            
