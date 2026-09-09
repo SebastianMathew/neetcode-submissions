1c9566a100051be8e13e92841e23fcb140f757af
SebastianMathew <139248438+SebastianMathew@users.noreply.github.com>
2026-09-09 12:52:30 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+
+            seen.add(i)
+        return False
